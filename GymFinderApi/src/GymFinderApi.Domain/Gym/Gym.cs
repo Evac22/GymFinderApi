073720253b5d8f4b728153b34d6124efbac78233
Vlad Chubarov <chubarov205@gmail.com>
2026-02-05 21:16:48 +0200

    Add Gym

---
+++ GymFinderApi/src/GymFinderApi.Domain/Gym/Gym.cs
@@ -0,0 +1,16 @@
+﻿namespace GymFinderApi.Domain.Gym;
+
+public class Gym
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Name { get; set; } 
+    public string Description { get; set; } = string.Empty;
+    public string CreatedByUserId { get; set; } = string.Empty;
+    public string OwnerUserId { get; set; } = string.Empty;
+    public Guid CoverPhotoId { get; set; }
+    public string Schedule { get; set; } = string.Empty;
+    public required string Address { get; set; } 
+    public required string City { get; set; }
+    public DateTime CreatedAt { get; set; } // дата и время создания сущности
+    public bool Status { get; set; } 
+}
