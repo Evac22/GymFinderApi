aca4766ce013e1feea4a8ee541e488e5d4532968
Vlad Chubarov <chubarov205@gmail.com>
2026-02-06 18:30:39 +0200

    Add Comment

---
+++ GymFinderApi/src/GymFinderApi.Domain/Comment/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace GymFinderApi.Domain.Comment;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    public Guid GymId { get; set; }
+    public Guid UserId { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public Guid? ParentCommentId { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ GymFinderApi/src/GymFinderApi.Domain/Comment/CommentPhoto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace GymFinderApi.Domain.Comment;
+
+public class CommentPhoto
+{
+    public Guid Id { get; set; }
+    public Guid CommentId { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+}
